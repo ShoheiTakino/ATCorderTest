bfe42f94a67bc0d92b591e968793763dc40d63b4
ShoheiTakino <shohei0522penguin@gmail.com>
2023-12-10 10:15:36 +0900

    ATCoder

--- ATCorderTest/main.swift
+++ ATCorderTest/main.swift
@@ -7,5 +7,39 @@
 
 import Foundation
 
-print("Hello, World!")
+func readInt() -> Int {
+    return Int(readLine()!)!
+}
 
+
+func readStrings() -> [String] {
+    return readLine()!.split(separator: "").map { String($0) }
+}
+
+func aa() {
+    let N = readInt()
+    let S = readStrings()
+    var abc = "ABC"
+
+    for i in 0..<N {
+        print(S[i])
+        if S[i] == "A" {
+            abc = abc.filter { $0 == "A" }
+            if S[i + 1] == "B" {
+                abc = abc.filter { $0 == "B" }
+                if S[i + 2] == "C" {
+                    abc = abc.filter { $0 == "C" }
+                    print(i)
+                    return
+                }
+                abc = "ABC"
+                break
+            }
+            abc = "ABC"
+            break
+        }
+    }
+    print(-1)
+}
+
+aa()
