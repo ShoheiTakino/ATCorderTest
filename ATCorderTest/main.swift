6b881af88a202a827ad84662e102abd013b2d610
ShoheiTakino <shohei0522penguin@gmail.com>
2024-01-04 22:42:58 +0900

    A - Count Down

--- ATCorderTest/main.swift
+++ ATCorderTest/main.swift
@@ -1,15 +1,9 @@
 import Foundation
 
 func aaa() {
-//    let K = readInt()
-//    var answerList: [String] =  []
-//    for i in 1...K {
-//        answerList.append(alphabetForNumber(i))
-//    }
-//    print(answerList.joined())
-    let K = readInt()
-    let answerList = (1...K).map { alphabetForNumber($0) }
-    print(answerList.joined())
+    let N = readInt()
+    let answerList: [String] = (0...N).map { String($0) }.reversed()
+    answerList.forEach { print($0) }
 }
 
 aaa()
@@ -20,18 +14,9 @@ func readInt() -> Int {
     return Int(readLine()!)!
 }
 
-func alphabetForNumber(_ number: Int) -> String {
-    guard (1...26).contains(number) else {
-        return ""
-    }
-
-    let unicodeValue = UnicodeScalar("A").value + UInt32(number) - 1
-    return String(UnicodeScalar(unicodeValue)!)
-}
-
 // 標準入力
 
 /*
  input
- 3
+ 22
  */
