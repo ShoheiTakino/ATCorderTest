e7798c57fed8743baabd04a02137cec9099edca4
ShoheiTakino <shohei0522penguin@gmail.com>
2024-01-06 10:04:10 +0900

    B - Yellow and Red Card

--- ATCorderTest/main.swift
+++ ATCorderTest/main.swift
@@ -1,17 +1,28 @@
 import Foundation
 
 func aaa() {
-    let (H, _) = readTwoInts()
-    let stringList: [String] = (0..<H).map { _ in readLine()! }
-    let answer = stringList.flatMap { $0.map { $0 == "#" ? 1 : 0 } }.reduce(0, +)
-//    var answer = 0
-//    for i in 0..<stringList.count {
-//        for j in 0..<W {
-//            if Array(stringList[i])[j] != "#" { continue }
-//            answer += 1
-//        }
-//    }
-    print(answer)
+    let (N, Q) = readTwoInts()
+    // player管理用の[event,x]を生成する2次元の文字列配列を生成する
+    var player: [[Int]] = []
+    for i in 1...N {
+        player.append([0,i])
+    }
+
+    for _ in 0..<Q {
+        let (event, x) = readTwoInts() // eventがカード等、xが人
+        if event == 3 {
+            // xの現在の累積カードの状況を確認して、2以上であればYesをそれ以外はNoを出力する
+            let card = player.filter { $0[1] == x }.map { $0[0] }.compactMap { Int($0) }.first
+            print(card! > 1 ? "Yes" : "No")
+        } else {
+            for i in 0..<player.count {
+                // xが同じ配列に対して、eventで与えられたカードを累積していく
+                if player[i][1] == x {
+                    player[i][0] += event
+                }
+            }
+        }
+    }
 }
 
 aaa()
@@ -28,8 +39,14 @@ func readTwoInts() -> (a: Int, b: Int) {
 
 /*
  input
- 3 5
- #....
- .....
- .##..
+ 3 9
+ 3 1
+ 3 2
+ 1 2
+ 2 1
+ 3 1
+ 3 2
+ 1 2
+ 3 2
+ 3 3
  */
