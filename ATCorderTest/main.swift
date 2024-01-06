737034516080d1bbeef738d46980370fe664d954
ShoheiTakino <shohei0522penguin@gmail.com>
2024-01-06 15:08:02 +0900

    B - Postal Card

--- ATCorderTest/main.swift
+++ ATCorderTest/main.swift
@@ -2,11 +2,32 @@ import Foundation
 
 func aaa() {
     let (N, K) = readTwoInts()
-    // 指定された順位までの文字列を使用した文字列配列を生成
-    let stringList: [String] = (0..<N).map { _ in readLine()! }.enumerated().filter { index, _ in index < K }.map { _, value in value }
-    // 生成した文字列配列を辞書順に変更して、1つずつ出力する
-    stringList.sorted().forEach { print($0) }
+    let SList: [String] = (0..<N).map { _ in getLastThreeCharacters(readLine()!) }
+    let TList: [String] = (0..<K).map { _ in readLine()! }
+    var answer = 0
+    for s in SList {
+        for t in TList {
+            if s == t {
+                answer += 1
+                break
+            }
+        }
+    }
+    print(answer)
 }
+
+// こちらの方が処理速度は遅い
+//func aaa() {
+//    let (N, K) = readTwoInts()
+//    let SList: [String] = (0..<N).map { _ in getLastThreeCharacters(readLine()!) }
+//    let TList: [String] = (0..<K).map { _ in readLine()! }
+//
+//    let answer = SList.reduce(0) { result, upper in
+//        return result + (TList.contains { lower in upper.contains(lower) } ? 1 : 0)
+//    }
+//
+//    print(answer)
+//}
 
 aaa()
 
@@ -17,15 +38,21 @@ func readTwoInts() -> (a: Int, b: Int) {
     return (a: ints[0], b: ints[1])
 }
 
+func getLastThreeCharacters(_ str: String) -> String {
+    let startIndex = str.index(str.endIndex, offsetBy: -3)
+    let lastThreeCharacters = str[startIndex...]
+    return String(lastThreeCharacters)
+}
 
 // 標準入力
 
 /*
  input 頭のスペースに注意
-5 3
-abc
-aaaaa
-xyz
-a
-def
+3 3
+142857
+004159
+071028
+159
+287
+857
  */
