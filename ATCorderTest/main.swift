5839b22415d4ba1a984c35743f554255f5b6fb7f
ShoheiTakino <shohei0522penguin@gmail.com>
2024-01-07 15:34:27 +0900

    B - Inverse Prefix Sum

--- ATCorderTest/main.swift
+++ ATCorderTest/main.swift
@@ -2,22 +2,17 @@ import Foundation
 
 func aaa() {
     let _ = readInt()
-    var answerList = readInts()
-    let Q = readInt()
-    let queryList = (0..<Q).map { _ in readInts() }
-    for query in queryList {
-        // 配列内の指定の数字を置換する処理を追加
-        if query[0] == 1 {
-            answerList[query[1] - 1] = query[2]
-            // query 2の場合は出力のため出力だけして終了
-        } else if query[0] == 2 {
-            print(answerList[query[1] - 1])
-        }
+    var intList = readInts()
+    for i in 0..<intList.count {
+        if i == 0 { continue }
+        let sum = intList[0..<i].reduce(0, +)
+        let diffInt = intList[i] - sum
+        intList[i] = diffInt
     }
+    print(intList.map { String($0) }.joined(separator: " "))
 }
 
 aaa()
-
 
 func readInt() -> Int {
     return Int(readLine()!)!
@@ -31,14 +26,6 @@ func readInts() -> [Int] {
 
 /*
  input 頭のスペースに注意
- 3
- 1 3 5
- 7
- 2 2
- 2 3
- 1 3 0
- 2 3
- 1 2 8
- 2 2
- 2 1
+3
+3 4 8
  */
