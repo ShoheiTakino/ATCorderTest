9860fe0a2ca4d28e4b9b1561430d99db445fcd65
ShoheiTakino <shohei0522penguin@gmail.com>
2024-01-07 21:11:00 +0900

    B - LOOKUP

--- ATCorderTest/main.swift
+++ ATCorderTest/main.swift
@@ -1,31 +1,18 @@
 import Foundation
 
 func aaa() {
-    let _ = readInt()
-    var intList = readInts()
-    for i in 0..<intList.count {
-        if i == 0 { continue }
-        let sum = intList[0..<i].reduce(0, +)
-        let diffInt = intList[i] - sum
-        intList[i] = diffInt
-    }
-    print(intList.map { String($0) }.joined(separator: " "))
+    let S = readLine()!
+    let T = readLine()!
+    print(S.contains(T) ? "Yes" : "No")
 }
 
 aaa()
 
-func readInt() -> Int {
-    return Int(readLine()!)!
-}
-
-func readInts() -> [Int] {
-    return readLine()!.split(separator: " ").map { Int(String($0))! }
-}
 
 // 標準入力
 
 /*
  input 頭のスペースに注意
-3
-3 4 8
+voltage
+tag
  */
