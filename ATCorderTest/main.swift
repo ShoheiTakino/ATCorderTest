6544bcac877e33633e14435b03c65aa7096a600b
ShoheiTakino <shohei0522penguin@gmail.com>
2024-01-08 21:40:46 +0900

    A - 1-2-4 Test

--- ATCorderTest/main.swift
+++ ATCorderTest/main.swift
@@ -1,8 +1,16 @@
 import Foundation
 
 func aaa() {
-    let answer = String(format: "%X", (readInt()))
-    print(answer.count == 2 ? answer : "0" + answer)
+    let (A, B) = readTwoInts()
+    var answerList: [Int] = []
+    if A == 0 && B == 0 {
+        print(0)
+        return
+    }
+
+    answerList = getTargetList(A)
+    answerList += getTargetList(B)
+    print(Array(Set(answerList)).reduce(0, +))
 }
 
 aaa()
@@ -10,12 +18,29 @@ aaa()
 
 // テンプレ
 
+func getTargetList(_ int: Int) -> [Int] {
+    if int == 3 {
+        return [1, 2]
+    } else if int == 5 {
+        return [1, 4]
+    } else if int == 6 {
+        return [2, 4]
+    }
+
+    return [int]
+}
+
 func readInt() -> Int {
     return Int(readLine()!)!
+}
+
+func readTwoInts() -> (a: Int, b: Int) {
+    let ints = readLine()!.split(separator: " ").map { Int(String($0))! }
+    return (a: ints[0], b: ints[1])
 }
 
 
 /*
  input 頭のスペースに注意
-99
+1 2
  */
