dcca2326ee5ae347a01171850d763702620be8a8
ShoheiTakino <shohei0522penguin@gmail.com>
2024-01-10 20:35:54 +0900

    A - 202<s>3</s>

--- ATCorderTest/main.swift
+++ ATCorderTest/main.swift
@@ -1,41 +1,10 @@
 import Foundation
 
 func aaa() {
-    let (N, M) = readTwoInts()
-    var cList = readStringListWithSpace()
-    var dList = readStringListWithSpace()
-    var pList = readInts()
-    var answer = 0
-    for i in 0..<N {
-        var price = pList[0]
-        for j in 1...M {
-            if cList[i] == dList[j - 1] {
-                price = pList[j]
-            }
-        }
-        answer += price
-    }
-    print(answer)
+    print(readLine()!.dropLast() + "4")
 }
 
 aaa()
-
-// テンプレ
-
-func readTwoInts() -> (a: Int, b: Int) {
-    let ints = readLine()!.split(separator: " ").map { Int(String($0))! }
-    return (a: ints[0], b: ints[1])
-}
-
-func readInts() -> [Int] {
-    return readLine()!.split(separator: " ").map { Int(String($0))! }
-}
-
-func readStringListWithSpace() -> [String] {
-    return readLine()!.split(separator: " ").map { String($0) }
-}
-
-
 
 /*
  input 頭のスペースに注意
