0e4432913d42a7176331b1020b55c54acece2690
ShoheiTakino <shohei0522penguin@gmail.com>
2024-01-12 22:27:15 +0900

    A - Intersection

--- ATCorderTest/main.swift
+++ ATCorderTest/main.swift
@@ -1,16 +1,12 @@
 import Foundation
 
 func aaa() {
-    let Y = readInt()
-    if Y % 4 == 2 {
-        print(Y)
-    } else if Y % 4 == 1 {
-        print(Y + 1)
-    } else if Y % 4 == 0 {
-        print(Y + 2)
-    } else if Y % 4 == 3 {
-        print(Y + 3)
-    }
+    let inputArray = readLine()!.split(separator: " ").map { Int($0)! }
+    let l1 = inputArray[0]
+    let r1 = inputArray[1]
+    let l2 = inputArray[2]
+    let r2 = inputArray[3]
+    print(max(0, min(r1, r2) - max(l1, l2))) // これでも良い
 }
 
 
@@ -18,12 +14,12 @@ aaa()
 
 // テンプレ
 
-func readInt() -> Int {
-    return Int(readLine()!)!
+func readInts() -> [Int] {
+    return readLine()!.split(separator: " ").map { Int(String($0))! }
 }
 
 
 /*
  input 頭のスペースに注意
-
+ 0 3 1 5
  */
