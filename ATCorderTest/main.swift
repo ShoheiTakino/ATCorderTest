bf22e30ba82382defbe2a0ca4a3c264e6d4ea303
ShoheiTakino <shohei0522penguin@gmail.com>
2024-01-14 19:49:25 +0900

    B - Minimize Ordering

--- ATCorderTest/main.swift
+++ ATCorderTest/main.swift
@@ -1,37 +1,11 @@
 import Foundation
 
 func aaa()  {
-    let N = readInt()
-    let AList = readInts()
-    let setAList = Set(AList)
-    let BList = readInts()
-    var aAnswer = 0
-    var bAnswer = 0
-    for i in 0..<AList.count {
-
-        if AList[i] == BList[i] {
-            aAnswer += 1
-            continue
-        }
-
-        if setAList.contains(BList[i]) {
-            bAnswer += 1
-        }
-    }
-
-    print(aAnswer, separator: "\n")
-    print(bAnswer)
+    print(String(readLine()!.sorted()))
 }
 
 aaa()
 
-func readInt() -> Int {
-    return Int(readLine()!)!
-}
-
-func readInts() -> [Int] {
-    return readLine()!.split(separator: " ").map { Int(String($0))! }
-}
 /*
  input 頭のスペースに注意
 4
