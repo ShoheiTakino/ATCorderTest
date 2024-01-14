fec9b06a24bb7970fbd7cf9aa709267364e30a1b
ShoheiTakino <shohei0522penguin@gmail.com>
2024-01-14 20:31:56 +0900

    B - Pasta

--- ATCorderTest/main.swift
+++ ATCorderTest/main.swift
@@ -1,15 +1,41 @@
 import Foundation
 
 func aaa()  {
-    print(String(readLine()!.sorted()))
+    let (N, _) = readTwoInts()
+    let A = readInts()
+    var B = readInts()
+    for i in 0..<N {
+        if B.isEmpty {
+            print("Yes")
+            return
+        }
+
+        if B.contains(A[i]) {
+            if let index = B.firstIndex(of: A[i]) {
+                B.remove(at: index)
+            }
+        }
+    }
+    print(B.isEmpty ? "Yes" : "No")
 }
 
 aaa()
 
+
+func readInts() -> [Int] {
+    return readLine()!.split(separator: " ").map { Int(String($0))! }
+}
+
+
+func readTwoInts() -> (a: Int, b: Int) {
+    let ints = readLine()!.split(separator: " ").map { Int(String($0))! }
+    return (a: ints[0], b: ints[1])
+}
+
 /*
  input 頭のスペースに注意
-4
-1 3 5 2
-2 3 1 4
+3 2
+1 1 3
+3 1
  */
 
