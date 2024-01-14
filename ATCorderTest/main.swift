6d3da9d217e99ef817e10793875155cb5673a205
ShoheiTakino <shohei0522penguin@gmail.com>
2024-01-14 22:14:44 +0900

    A - Long Loong

--- ATCorderTest/main.swift
+++ ATCorderTest/main.swift
@@ -1,41 +1,20 @@
 import Foundation
 
 func aaa()  {
-    let (N, _) = readTwoInts()
-    let A = readInts()
-    var B = readInts()
-    for i in 0..<N {
-        if B.isEmpty {
-            print("Yes")
-            return
-        }
-
-        if B.contains(A[i]) {
-            if let index = B.firstIndex(of: A[i]) {
-                B.remove(at: index)
-            }
-        }
-    }
-    print(B.isEmpty ? "Yes" : "No")
+    let N = readInt()
+    let o = (0..<N).map { _ in "o" }.joined()
+    print("L\(o)ng")
 }
 
 aaa()
 
-
-func readInts() -> [Int] {
-    return readLine()!.split(separator: " ").map { Int(String($0))! }
-}
-
-
-func readTwoInts() -> (a: Int, b: Int) {
-    let ints = readLine()!.split(separator: " ").map { Int(String($0))! }
-    return (a: ints[0], b: ints[1])
+func readInt() -> Int {
+    return Int(readLine()!)!
 }
 
 /*
  input 頭のスペースに注意
-3 2
-1 1 3
-3 1
+5
+2 2 3 1 1
  */
 
