981f750067eb33c5f1e848833e53cf8061f98e93
ShoheiTakino <shohei0522penguin@gmail.com>
2024-01-16 21:06:37 +0900

    A - 2^N

--- ATCorderTest/main.swift
+++ ATCorderTest/main.swift
@@ -1,19 +1,9 @@
 import Foundation
 
 func aaa() {
-    if let b = Int(readLine()!) {
-        for a in 1...15 {
-            var x = 1
-            for _ in 0..<a {
-                x *= a
-            }
-            if x == b {
-                print(a)
-                return
-            }
-        }
-        print(-1)
-    }
+    let N = Int(readLine()!)!
+    let pow = pow(Double(2), Double(N))
+    print(Int(pow))
 }
 
 aaa()
