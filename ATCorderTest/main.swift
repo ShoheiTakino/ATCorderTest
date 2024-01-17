dd43a1ec4cd0b249aec3f07e3605fc2da5281e75
ShoheiTakino <shohei0522penguin@gmail.com>
2024-01-17 19:32:04 +0900

    B - Roulette

--- ATCorderTest/main.swift
+++ ATCorderTest/main.swift
@@ -1,7 +1,37 @@
 import Foundation
 
 func aaa() {
-    print(String(readInt()).dropFirst())
+    let K = readInt()
+    var intList: [[Int]] = []
+    var answerIntList: [[Int:Int]] = []
+    for _ in 0..<K {
+        let _ = readInt()
+        intList.append(readInts())
+    }
+    let X = readInt()
+    var minimum = 38
+    for i in 0..<K {
+        if intList[i].contains(X) {
+            answerIntList.append([i + 1: intList[i].count]) // [i:j] i:何番目か,j:何個かけたか
+            minimum = min(minimum, intList[i].count)
+        }
+    }
+    
+    // 当たった人が誰もいない場合
+    if answerIntList.isEmpty {
+        print(0)
+        return
+    }
+
+    var aa : [String] = []
+    for element in answerIntList {
+        for (key, value) in element {
+            if value != minimum { continue }
+            aa.append("\(key)")
+        }
+    }
+    print(aa.count)
+    print(aa.joined(separator: " "))
 }
 
 aaa()
@@ -23,6 +53,15 @@ func readInts() -> [Int] {
 
 /*
  input 頭のスペースに注意
-254
+4
+3
+7 19 20
+4
+4 19 24 0
+2
+26 10
+3
+19 31 24
+19
  */
 
