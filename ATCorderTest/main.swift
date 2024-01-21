17e1b8833bcfac9407de5408d62e82947320c857
ShoheiTakino <shohei0522penguin@gmail.com>
2024-01-21 14:03:40 +0900

    B - Extended ABC

--- ATCorderTest/main.swift
+++ ATCorderTest/main.swift
@@ -1,25 +1,17 @@
 import Foundation
 
 func aaa() {
-    let N = readInt()
-    var aoki = 0
-    var takahashi = 0
-    for _ in 0..<N {
-        let iScore = readInts()
-        takahashi += iScore[0]
-        aoki += iScore[1]
-    }
-
-    if takahashi == aoki {
-        print("Draw")
-        return
-    } else if takahashi > aoki {
-        print("Takahashi")
-        return
-    } else if takahashi < aoki {
-        print("Aoki")
+    let S = readLine()!
+    let stringArray = Array(S)
+    for i in 1..<stringArray.count {
+        let str = stringArray[i - 1]
+        let iStr = stringArray[i]
+        let unicode = UnicodeScalar(String(stringArray[i]))!.value
+        if str <= iStr { continue }
+        print("No")
         return
     }
+    print("Yes")
 }
 
 aaa()
@@ -27,12 +19,6 @@ aaa()
 func readInt() -> Int {
     return Int(readLine()!)!
 }
-
-func readInts() -> [Int] {
-    return readLine()!.split(separator: " ").map { Int(String($0))! }
-}
-
-
 
 /*
  input 頭のスペースに注意
