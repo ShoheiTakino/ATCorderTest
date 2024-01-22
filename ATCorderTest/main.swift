150e349ecaa3a5d7c74e34583c778db3ed20e887
ShoheiTakino <shohei0522penguin@gmail.com>
2024-01-22 20:16:02 +0900

    A - Adjacent Squares

--- ATCorderTest/main.swift
+++ ATCorderTest/main.swift
@@ -1,31 +1,26 @@
 import Foundation
 
 func aaa() {
-    let N = readInt()
-    let S = readStrings()
-    for i in 1..<N {
-        var answer = 0
-        for j in i...N - 1 {
-            if S[j] == S[j - i] { break }
-            answer += 1
-        }
-        print(answer)
-    }
+    let (H, W) = readTwoInts()
+    let (R, C) = readTwoInts()
+    var answer = 0
+    if C != 1 { answer += 1 }
+    if C != W { answer += 1 }
+    if R != 1 { answer += 1 }
+    if R != H { answer += 1 }
+    print(answer)
 }
 
 aaa()
 
-func readInt() -> Int {
-    return Int(readLine()!)!
-}
-
-func readStrings() -> [String] {
-    return readLine()!.split(separator: "").map { String($0) }
+func readTwoInts() -> (a: Int, b: Int) {
+    let ints = readLine()!.split(separator: " ").map { Int(String($0))! }
+    return (a: ints[0], b: ints[1])
 }
 
 /*
  input 頭のスペースに注意
-6
-abcbac
+3 4
+1 3
  */
 
