bb4176ba171e384a5818f05665ae9b96d9c10ce4
ShoheiTakino <shohei0522penguin@gmail.com>
2024-01-26 19:24:09 +0900

    B - Distance Between Tokens

--- ATCorderTest/main.swift
+++ ATCorderTest/main.swift
@@ -2,13 +2,33 @@ import Foundation
 
 func aaa() {
     let (H, W) = readTwoInts()
-    let (R, C) = readTwoInts()
-    var answer = 0
-    if C != 1 { answer += 1 }
-    if C != W { answer += 1 }
-    if R != 1 { answer += 1 }
-    if R != H { answer += 1 }
-    print(answer)
+    let listInList = (0..<H).map { _ in readStrings() }
+    var firstIndex: [Int] = []
+    var secondIndex: [Int] = []
+
+    for i in 0..<H {
+        let iList = listInList[i]
+        for _ in 0..<W {
+            if let index = iList.lastIndex(of: "o") {
+                if firstIndex.isEmpty {
+                    firstIndex = [i, index]
+
+                    let filtered = iList.filter { $0 == "-" }
+                    print(filtered, "aa白木")
+                    break
+                }
+
+                if secondIndex.isEmpty {
+                    secondIndex = [i, index]
+                    break
+                }
+            }
+        }
+    }
+
+    let raw = abs(secondIndex[0] - firstIndex[0])
+    let line = abs(firstIndex[1] - secondIndex[1])
+    print(raw + line)
 }
 
 aaa()
@@ -18,9 +38,15 @@ func readTwoInts() -> (a: Int, b: Int) {
     return (a: ints[0], b: ints[1])
 }
 
+func readStrings() -> [String] {
+    return readLine()!.split(separator: "").map { String($0) }
+}
+
+
 /*
  input 頭のスペースに注意
-3 4
-1 3
+2 3
+--o
+o--
  */
 
