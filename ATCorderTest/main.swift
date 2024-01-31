5f74bfec441898838d3fc7e997ca317fff7bf9d9
ShoheiTakino <shohei0522penguin@gmail.com>
2024-01-31 23:44:08 +0900

    last latter

--- ATCorderTest/main.swift
+++ ATCorderTest/main.swift
@@ -1,27 +1,15 @@
 import Foundation
 
 func aaa() {
-    var list = readInts()
-    var answer = ""
-    var count = 0
-    while list[0] >= 0 {
-        count += 1
-        if count % 3 == 0 {
-            list[0] -= list[3]
-            answer = "T"
-        } else if count % 3 == 1 {
-            list[0] -= list[1]
-            answer = "F"
-        } else if count % 3 == 2 {
-            list[0] -= list[2]
-            answer = "M"
-        }
-    }
-
-    print(answer)
+    let _ = readInt()
+    print(readLine()!.last!)
 }
 
 aaa()
+
+func readInt() -> Int {
+    return Int(readLine()!)!
+}
 
 func readInts() -> [Int] {
     return readLine()!.split(separator: " ").map { Int(String($0))! }
