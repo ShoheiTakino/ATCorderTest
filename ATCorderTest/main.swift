61ac69e0d1deb7e92dd25ff0748c92c807bad040
ShoheiTakino <shohei0522penguin@gmail.com>
2024-02-06 18:47:12 +0900

    A - Rotate

--- ATCorderTest/main.swift
+++ ATCorderTest/main.swift
@@ -1,27 +1,21 @@
 import Foundation
 
 func aaa() {
-    var S = readStrings()
-    let (a, b) = readTwoInts()
-    S.swapAt(a - 1, b - 1)
-    print(S.joined(separator: ""))
+    var abcList = readStrings()
+    let bca = Int([abcList[1], abcList[2], abcList[0]].joined(separator: ""))!
+    let cab = Int([abcList[2], abcList[0], abcList[1]].joined(separator: ""))!
+    let abc = Int(abcList.joined(separator: ""))!
+    print(abc + bca + cab)
 }
 
 aaa()
-
-func readTwoInts() -> (a: Int, b: Int) {
-    let ints = readLine()!.split(separator: " ").map { Int(String($0))! }
-    return (a: ints[0], b: ints[1])
-}
 
 func readStrings() -> [String] {
     return readLine()!.split(separator: "").map { String($0) }
 }
 
-
 /*
  input 頭のスペースに注意
-chokudai
-3 5
+123
  */
 
