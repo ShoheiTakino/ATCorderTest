8610ceb1d4aeaa7f5b71573c80885a58beaab9be
ShoheiTakino <shohei0522penguin@gmail.com>
2024-02-07 23:01:42 +0900

    A - Weird Function

--- ATCorderTest/main.swift
+++ ATCorderTest/main.swift
@@ -1,21 +1,22 @@
 import Foundation
 
 func aaa() {
-    var abcList = readStrings()
-    let bca = Int([abcList[1], abcList[2], abcList[0]].joined(separator: ""))!
-    let cab = Int([abcList[2], abcList[0], abcList[1]].joined(separator: ""))!
-    let abc = Int(abcList.joined(separator: ""))!
-    print(abc + bca + cab)
+    let t = readInt()
+    print(a(a(a(t) + t) + a(a(t))))
+}
+
+private func a(_ t: Int) -> Int {
+    t * t + 2 * t + 3
 }
 
 aaa()
 
-func readStrings() -> [String] {
-    return readLine()!.split(separator: "").map { String($0) }
+func readInt() -> Int {
+    return Int(readLine()!)!
 }
 
 /*
  input 頭のスペースに注意
-123
+0
  */
 
