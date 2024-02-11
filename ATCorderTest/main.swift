e3a2c5b52ccc8a6bd7acd9146f507f90eac9d0b5
ShoheiTakino <shohei0522penguin@gmail.com>
2024-02-11 22:31:38 +0900

    A - QQ solver

--- ATCorderTest/main.swift
+++ ATCorderTest/main.swift
@@ -1,38 +1,18 @@
 import Foundation
 
 func aaa() {
-    var Q = readInt()
-    var list = (0..<Q).map { _ in readInts() }
-    var answer: [Int] = []
-    for i in 0..<list.count {
-        let i = list[i]
-        if i[0] == 1 {
-            answer.append(i[1])
-        } else if i[0] == 2 {
-            answer.reverse()
-            print(answer[i[1] - 1])
-            answer.reverse()
-        }
-    }
+    let S = readStrings()
+    print(Int(S[0])! * Int(S[2])!)
 }
 
 aaa()
 
-func readInt() -> Int {
-    return Int(readLine()!)!
-}
-
-func readInts() -> [Int] {
-    return readLine()!.split(separator: " ").map { Int(String($0))! }
+func readStrings() -> [String] {
+    return readLine()!.split(separator: "").map { String($0) }
 }
 
 /*
  input 頭のスペースに注意
-5
-1 20
-1 30
-2 1
-1 40
-2 3
+3x7
  */
 
